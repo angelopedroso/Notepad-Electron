a18bc4cc12bb22eb39fad15f91d2ebfd224b8f42
Angelo <solonasusonly0@gmail.com>
2025-02-05 15:47:42 -0300

    v1.1.21

--- forge.config.ts
+++ forge.config.ts
@@ -72,18 +72,6 @@ const config: ForgeConfig = {
       [FuseV1Options.OnlyLoadAppFromAsar]: true,
     }),
   ],
-  publishers: [
-    {
-      name: '@electron-forge/publisher-github',
-      config: {
-        repository: {
-          owner: 'angelopedroso',
-          name: 'Notepad-Electron',
-        },
-        authToken: process.env.GH_TOKEN,
-      },
-    },
-  ],
 }
 
 export default config
