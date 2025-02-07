6611d7aa8143e29ff85fed4722678c7ebeba8926
Angelo <solonasusonly0@gmail.com>
2025-02-07 11:32:56 -0300

    feat: create trash route

--- src/renderer/routes.tsx
+++ src/renderer/routes.tsx
@@ -3,6 +3,7 @@ import { MemoryRouter, Route, Routes as RouterRoutes } from 'react-router-dom'
 import { Blank } from './src/pages/blank'
 import { Document } from './src/pages/document'
 import { Default } from './src/pages/layout/default'
+import { Trash } from './src/pages/trash'
 
 export function Routes() {
   return (
@@ -11,6 +12,7 @@ export function Routes() {
         <Route path="/" element={<Default />}>
           <Route path="/" element={<Blank />} />
           <Route path="/documents/:id" element={<Document />} />
+          <Route path="/trash" element={<Trash />} />
         </Route>
       </RouterRoutes>
     </MemoryRouter>
---
+++ src/renderer/src/pages/trash.tsx
@@ -0,0 +1,3 @@
+export function Trash() {
+  return <h1>Reton</h1>
+}
